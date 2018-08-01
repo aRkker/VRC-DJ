d72ed2d44b39a739e5b7b053d7f7216629ddfde6
aRkker <arkkerx@gmail.com>
2018-08-01 13:02:51 +0300

    Conversion done

--- commands.js
+++ commands.js
@@ -1,5 +1,7 @@
 const url = require('url'); // for URL validation
-const youtubedl = require('youtube-dl');
+const youtubedl = require('youtube-dl'); // duh
+const fs = require('fs'); // filesystem for file writing
+const ffmpeg = require('fluent-ffmpeg');
 
 module.exports = {
     
@@ -17,22 +19,47 @@ module.exports = {
 
                 for (let format of info.formats) {
                     if (format.ext === "mp4") {
-                       // console.log('Got one MP4 at least');
-                        console.log(format);
-                        bestURL = format.url;
+                      bestURL = format.url;
                     }
                 }
 
                 // Didn't find an MP4 one!
                 if (bestURL === 'none') {
-                    myMessage.edit('Could not find a suitable URL to store, converting. This will take a moment', 'No valid URL in youtube metadata');
+                    myMessage.edit('Could not find a suitable URL to store, converting. This will take a moment\r\nDownloading video...', 'No valid URL in youtube metadata');
                     // We must do conversion
+                    handleConversion(params[0], myMessage);
                 }
                 else {
-                    // Do what ever here with bestURL, store it to the database or however it works
+                    // TODO: Do what ever here with bestURL, store it to the database or however it works
                     myMessage.edit('Added the track to the list!');
                 }
             });
         });
-    }
+    },
+
+
+}
+
+handleConversion = (url, message) => {
+    let video = youtubedl(url);
+
+    // Pipe it to an actual file
+    video.pipe(fs.createWriteStream('C:\\temp\\convert-me.mp4')); // TODO: change path
+
+    video.on('end', () => {
+        message.edit('Could not find a suitable URL to store, converting. This will take a moment\r\nConverting video...');
+
+        ffmpeg('C:\\temp\\convert-me.mp4')
+        .audioCodec('aac')
+        .videoCodec('libx264')
+        .on('error', err => {
+            console.log(err.message);
+        })
+        .on('end', () => {
+            message.edit('Added the track to the list!');
+            // TODO: actually store the file somewhere etc etc. Its in C:\temp\converted.mp4 now 
+        })
+        .save('C:\\temp\\converted.mp4'); // TODO: change path
+    });
+
 }
